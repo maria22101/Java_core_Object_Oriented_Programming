f5dbdbd0ce820c37be836e58606479a27c7eb147
Maria101 <maria.bilous101@gmail.com>
2019-11-19 22:33:43 +0200

    generateOceanMap() separated from displayOceanMap()

--- src/edx_javacore_2_module_1_project_Battle_ships/BattleShip.java
+++ src/edx_javacore_2_module_1_project_Battle_ships/BattleShip.java
@@ -2,14 +2,14 @@ package edx_javacore_2_module_1_project_Battle_ships;
 
 public class BattleShip {
 
-    static void displayOceanMap() {
+    static char[][] generateOceanMap() {
         char[][] oceanMap = new char[12][14];
         for (int i = 0; i < oceanMap.length; i++) {
             for (int j = 0; j < oceanMap[i].length; j++) {
 
                 if (i == 0 || i == oceanMap.length - 1) {
                     if (j != 0 && j != 1 && j != oceanMap[i].length - 1 && j != oceanMap[i].length - 2) {
-                            oceanMap[i][j] = (char) (j - 2 + '0');
+                        oceanMap[i][j] = (char) (j - 2 + '0');
                     }
                 } else {
                     if (j == 0 || j == oceanMap[i].length - 1) {
@@ -23,17 +23,21 @@ public class BattleShip {
 
             }
         }
+        return oceanMap;
+    }
 
-        for (int i = 0; i < oceanMap.length; i++) {
-            for (int j = 0; j < oceanMap[i].length; j++){
-                System.out.print(oceanMap[i][j]);
+    static void displayOceanMap(char[][] ch) {
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = 0; j < ch[i].length; j++) {
+                System.out.print(ch[i][j]);
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        displayOceanMap();
+        char [][] oceanMap = generateOceanMap();
+        displayOceanMap(oceanMap);
 
     }
 }
